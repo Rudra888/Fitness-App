32dab43965e0e073fb7c9c55c9f677eb425a02b8
Rudra888 <68020302+Rudra888@users.noreply.github.com>
2020-10-01 14:14:17 +0530

    Add files via upload

---
+++ fitness/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.rudra888.fitness
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+}
